98cb242d2f3531be5a67fe88b2e81a7efea836aa
gorzerk28 <onerrdogukan@gmail.com>
2026-02-17 15:44:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Sevgilin için site giriş şifresi
-  sitePassword: "iremhasekisultan.",
+  sitePassword: "iremhasekisultan",
   // Sadece sana özel admin panel şifresi
-  adminPassword: "gorzerk28.",
+  adminPassword: "gorzerk28",
 };
