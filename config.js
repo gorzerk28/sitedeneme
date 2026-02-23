79d78697add7edc177c2adb5ec045ffdcd08393f
gorzerk28 <onerrdogukan@gmail.com>
2026-02-24 01:36:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ window.APP_CONFIG = {
   ownerUsername: "kalpsorumlusu",
   ownerSitePassword: "gorzerk28",
   // Talep cevaplandığında bildirimin gideceği e-posta
-  partnerEmail: "kalpdefterimiz@gmail.com",
+  partnerEmail: "onerrdogukan@gmail.com",
   // Senkron modu:
   // local  -> sadece bu cihazda çalışır (farklı cihazda talep görünmez).
   // remote -> farklı cihazların aynı talepleri görmesi için zorunlu.
